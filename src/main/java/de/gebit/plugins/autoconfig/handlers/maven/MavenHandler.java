8469e2034c78aca58136a2f1ed36d7681e1ea82c
Julian Blazek <julian.blazek@gebit.de>
2024-07-19 16:49:45 +0200

    Mapping and resolving JDK versions
    - if a jdk isn't found on the system a notification suggests the user to fix the JDK differently

--- src/main/java/de/gebit/plugins/autoconfig/handlers/maven/MavenHandler.java
+++ src/main/java/de/gebit/plugins/autoconfig/handlers/maven/MavenHandler.java
@@ -15,7 +15,7 @@ import com.intellij.openapi.projectRoots.Sdk;
 import de.gebit.plugins.autoconfig.UpdateHandler;
 import de.gebit.plugins.autoconfig.handlers.AbstractHandler;
 import de.gebit.plugins.autoconfig.model.MavenConfiguration;
-import de.gebit.plugins.autoconfig.util.Notifications;
+import de.gebit.plugins.autoconfig.sdk.JDKResolver;
 import org.jetbrains.idea.maven.execution.MavenRunner;
 import org.jetbrains.idea.maven.project.MavenProjectBundle;
 import org.jetbrains.idea.maven.project.MavenProjectsManager;
@@ -53,35 +53,51 @@ public class MavenHandler extends AbstractHandler implements UpdateHandler<Maven
 		List<String> changedConfigs = new ArrayList<>();
 		var mavenProject = MavenProjectsManager.getInstance(project);
 		var mavenProjectSettings = mavenProject.getGeneralSettings();
-		applySetting(maven.getThreads(), mavenProjectSettings.getThreads(), mavenProjectSettings::setThreads, changedConfigs, "Maven thread parameter");
+		applySetting(maven.getThreads(), mavenProjectSettings.getThreads(), mavenProjectSettings::setThreads,
+				changedConfigs, "Maven thread parameter");
 
 		var settingsFilePath = maven.getSettingsFile();
 		if (settingsFilePath != null && project.getBasePath() != null) {
 			var projectBasePath = Paths.get(project.getBasePath());
 			var conversionContextImpl = new ConversionContextImpl(projectBasePath);
 			var expandPath = conversionContextImpl.expandPath(settingsFilePath);
-			applySetting(expandPath, mavenProjectSettings.getUserSettingsFile(), mavenProjectSettings::setUserSettingsFile, changedConfigs, "Maven user settings file");
+			applySetting(expandPath, mavenProjectSettings.getUserSettingsFile(),
+					mavenProjectSettings::setUserSettingsFile, changedConfigs, "Maven user settings file");
 		}
 
 		// https://youtrack.jetbrains.com/issue/IDEA-338870 setMavenHome und getMavenHome wird vsl. mit 2025.1 entfernt
-		applySetting(getMavenHome(maven.getUseMavenWrapper()), mavenProjectSettings.getMavenHome(), mavenProjectSettings::setMavenHome, changedConfigs, "Maven home");
-		applySetting(maven.getUseMavenConfig(), mavenProjectSettings.isUseMavenConfig(), mavenProjectSettings::setUseMavenConfig, changedConfigs, "Use maven config");
+		applySetting(getMavenHome(maven.getUseMavenWrapper()), mavenProjectSettings.getMavenHome(),
+				mavenProjectSettings::setMavenHome, changedConfigs, "Maven home");
+		applySetting(maven.getUseMavenConfig(), mavenProjectSettings.isUseMavenConfig(),
+				mavenProjectSettings::setUseMavenConfig, changedConfigs, "Use maven config");
 
 		var mavenImportingConfig = maven.getImporting();
 		if (mavenImportingConfig != null) {
 			var mavenImportingProjectSettings = mavenProject.getImportingSettings();
-			applySetting(mavenImportingConfig.getDetectCompiler(), mavenImportingProjectSettings.isAutoDetectCompiler(), mavenImportingProjectSettings::setAutoDetectCompiler, changedConfigs, "Autodetect compiler");
-			applySetting(mavenImportingConfig.getDownloadSources(), mavenImportingProjectSettings.isDownloadSourcesAutomatically(), mavenImportingProjectSettings::setDownloadSourcesAutomatically, changedConfigs, "Download sources");
-			applySetting(mavenImportingConfig.getDownloadDocumentation(), mavenImportingProjectSettings.isDownloadDocsAutomatically(), mavenImportingProjectSettings::setDownloadDocsAutomatically, changedConfigs, "Download documentation");
-			applySetting(mavenImportingConfig.getVmOptions(), mavenImportingProjectSettings.getVmOptionsForImporter(), mavenImportingProjectSettings::setVmOptionsForImporter, changedConfigs, "VM options for maven importer");
-			applySetting(findProjectSdk(mavenImportingConfig.getJdk(), project), mavenImportingProjectSettings.getJdkForImporter(), mavenImportingProjectSettings::setJdkForImporter, changedConfigs, "JDK for importer");
+			applySetting(mavenImportingConfig.getDetectCompiler(), mavenImportingProjectSettings.isAutoDetectCompiler(),
+					mavenImportingProjectSettings::setAutoDetectCompiler, changedConfigs, "Autodetect compiler");
+			applySetting(mavenImportingConfig.getDownloadSources(),
+					mavenImportingProjectSettings.isDownloadSourcesAutomatically(),
+					mavenImportingProjectSettings::setDownloadSourcesAutomatically, changedConfigs, "Download sources");
+			applySetting(mavenImportingConfig.getDownloadDocumentation(),
+					mavenImportingProjectSettings.isDownloadDocsAutomatically(),
+					mavenImportingProjectSettings::setDownloadDocsAutomatically, changedConfigs,
+					"Download documentation");
+			applySetting(mavenImportingConfig.getVmOptions(), mavenImportingProjectSettings.getVmOptionsForImporter(),
+					mavenImportingProjectSettings::setVmOptionsForImporter, changedConfigs,
+					"VM options for maven importer");
+			applySetting(findProjectSdk(mavenImportingConfig.getJdk(), project),
+					mavenImportingProjectSettings.getJdkForImporter(), mavenImportingProjectSettings::setJdkForImporter,
+					changedConfigs, "JDK for importer");
 		}
 
 		var mavenRunnerConfig = maven.getRunner();
 		if (mavenRunnerConfig != null) {
 			var mavenRunnerProjectSettings = MavenRunner.getInstance(project).getState();
-			applySetting(mavenRunnerConfig.getVmOptions(), mavenRunnerProjectSettings.getVmOptions(), mavenRunnerProjectSettings::setVmOptions, changedConfigs, "VM options for maven runner");
-			applySetting(findProjectSdk(mavenRunnerConfig.getJre(), project), mavenRunnerProjectSettings.getJreName(), mavenRunnerProjectSettings::setJreName, changedConfigs, "JRE for runner");
+			applySetting(mavenRunnerConfig.getVmOptions(), mavenRunnerProjectSettings.getVmOptions(),
+					mavenRunnerProjectSettings::setVmOptions, changedConfigs, "VM options for maven runner");
+			applySetting(findProjectSdk(mavenRunnerConfig.getJre(), project), mavenRunnerProjectSettings.getJreName(),
+					mavenRunnerProjectSettings::setJreName, changedConfigs, "JRE for runner");
 		}
 
 		return changedConfigs;
@@ -108,7 +124,9 @@ public class MavenHandler extends AbstractHandler implements UpdateHandler<Maven
 				return jdk.getName();
 			}
 		}
-		Notifications.showError("SDK \"" + sdkName + " not found, please install it and add it in your SDK list.", project);
-		return null;
+		
+		JDKResolver.resolveMissingJDK(sdkName, project);
+
+		return sdkName;
 	}
 }
